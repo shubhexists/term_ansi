219537201ba7c89a2a41d557466322e73603b6a5
Shubham Singh <shubh622005@gmail.com>
2024-09-21 15:23:30 +0530

    feat: added styles and bg

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
 //! # Color Formatting Crate
 //!
-//! This crate provides macros and functions for formatting terminal output with colors using ANSI escape codes. It supports various colors and allows nesting of colors for flexible and dynamic terminal output.
+//! This crate provides macros and functions for formatting terminal output with colors,
+//! background colors, and styles using ANSI escape codes. It supports various colors
+//! and allows nesting of colors for flexible and dynamic terminal output.
 //!
 //! ## Features
 //!
 //! - Apply predefined colors: Red, Green, Blue, White, Black, Yellow, Magenta, Cyan
-//! - Custom RGB color support
+//! - Apply background colors
+//! - Apply text styles: Bold, Italic, Underline
+//! - Custom RGB color support for text and background
+//! - HSL and HSV color support for text and background
 //! - Nested color support
 //!
 //! ## Usage
@@ -14,7 +19,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! term_ansi = "0.1.0"
+//! term_ansi = "0.2.0"
 //! ```
 //!
 //! Then you can use the provided macros to format your terminal output. Here are some examples:
@@ -25,27 +30,37 @@
 //! // Simple color application
 //! println!("{}", red!("Error message"));
 //!
-//! // Multiple colors in a single line
-//! println!("{}", red!("Error: {}, {}", blue!("File not found"), green!("Please check your path")));
+//! // Background color
+//! println!("{}", bg_blue!("Blue background"));
 //!
-//! // Nested colors
-//! println!("{}", white!("This is {} with {} color", red!("red"), green!("green")));
+//! // Styles
+//! println!("{}", bold!("Bold text"));
+//!
+//! // Combining color, background, and style
+//! println!("{}", red!("{}", bg_yellow!("{}", bold!("Warning: Important message"))));
 //!
 //! // Custom RGB color
-//! println!("{}", colour!(255, 0, 0, "This is red text"));
+//! println!("{}", rgb!(255, 0, 0, "This is red text"));
+//!
+//! // HSL and HSV colors
+//! println!("{}", hsl!(120.0, 1.0, 0.5, "This is green text"));
+//! println!("{}", bg_hsv!(240.0, 1.0, 1.0, "This has a blue background"));
 //! ```
 //!
 //! ## Macros
 //!
-//! - `red!`: Apply red color.
-//! - `green!`: Apply green color.
-//! - `blue!`: Apply blue color.
-//! - `white!`: Apply white color.
-//! - `black!`: Apply black color.
-//! - `yellow!`: Apply yellow color.
-//! - `magenta!`: Apply magenta color.
-//! - `cyan!`: Apply cyan color.
-//! - `colour!`: Apply a custom RGB color.
+//! ### Text Colors
+//! - `red!`, `green!`, `blue!`, `white!`, `black!`, `yellow!`, `magenta!`, `cyan!`
+//!
+//! ### Background Colors
+//! - `bg_red!`, `bg_green!`, `bg_blue!`, `bg_white!`, `bg_black!`, `bg_yellow!`, `bg_magenta!`, `bg_cyan!`
+//!
+//! ### Styles
+//! - `bold!`, `italic!`, `underline!`
+//!
+//! ### Custom Colors
+//! - `rgb!`, `bg_rgb!`: Apply custom RGB colors for text and background.
+//! - `hsl!`, `hsv!`, `bg_hsl!`, `bg_hsv!`: Apply colors using HSL or HSV color models for text and background.
 //!
 //! ## License
 //!
@@ -87,6 +102,54 @@ impl ColorContext {
     }
 }
 
+pub fn hsl_to_rgb(h: f64, s: f64, l: f64) -> (u8, u8, u8) {
+    let c: f64 = (1.0 - (2.0 * l - 1.0).abs()) * s;
+    let x: f64 = c * (1.0 - ((h / 60.0) % 2.0 - 1.0).abs());
+    let m: f64 = l - c / 2.0;
+
+    let (r, g, b): (f64, f64, f64) = match h {
+        h if h >= 0.0 && h < 60.0 => (c, x, 0.0),
+        h if h >= 60.0 && h < 120.0 => (x, c, 0.0),
+        h if h >= 120.0 && h < 180.0 => (0.0, c, x),
+        h if h >= 180.0 && h < 240.0 => (0.0, x, c),
+        h if h >= 240.0 && h < 300.0 => (x, 0.0, c),
+        h if h >= 300.0 && h <= 360.0 => (c, 0.0, x),
+        _ => (0.0, 0.0, 0.0),
+    };
+
+    let r: u8 = ((r + m) * 255.0).round() as u8;
+    let g: u8 = ((g + m) * 255.0).round() as u8;
+    let b: u8 = ((b + m) * 255.0).round() as u8;
+
+    (r, g, b)
+}
+
+pub fn hsv_to_rgb(h: f64, s: f64, v: f64) -> (u8, u8, u8) {
+    let c: f64 = v * s;
+    let x: f64 = c * (1.0 - ((h / 60.0) % 2.0 - 1.0).abs());
+    let m: f64 = v - c;
+
+    let (r, g, b): (f64, f64, f64) = match h {
+        h if h >= 0.0 && h < 60.0 => (c, x, 0.0),
+        h if h >= 60.0 && h < 120.0 => (x, c, 0.0),
+        h if h >= 120.0 && h < 180.0 => (0.0, c, x),
+        h if h >= 180.0 && h < 240.0 => (0.0, x, c),
+        h if h >= 240.0 && h < 300.0 => (x, 0.0, c),
+        h if h >= 300.0 && h <= 360.0 => (c, 0.0, x),
+        _ => (0.0, 0.0, 0.0),
+    };
+
+    let r: u8 = ((r + m) * 255.0).round() as u8;
+    let g: u8 = ((g + m) * 255.0).round() as u8;
+    let b: u8 = ((b + m) * 255.0).round() as u8;
+
+    (r, g, b)
+}
+
+pub fn reset_all() -> &'static str {
+    "\x1b[0m"
+}
+
 #[macro_export]
 /// Applies a color code to the provided format string.
 ///
@@ -110,7 +173,7 @@ impl ColorContext {
 macro_rules! apply_color {
     ($color_code:expr, $($arg:tt)*) => {{
         $crate::ColorContext::push($color_code);
-        let result = format!("{}{}", $color_code, format!($($arg)*));
+        let result = format!("{}{}{}", $color_code, format!($($arg)*), $crate::reset_all());
         $crate::ColorContext::pop();
         format!("{}{}", result, $crate::ColorContext::current_color())
     }};
@@ -291,11 +354,350 @@ macro_rules! cyan {
 /// ```
 /// use term_ansi::*;
 ///
-/// println!("{}", colour!(255, 0, 0, "This is {} text", "bright red"));
+/// println!("{}", rgb!(255, 0, 0, "This is {} text", "bright red"));
 /// ```
-macro_rules! colour {
+macro_rules! rgb {
     ($r:expr, $g:expr, $b:expr, $($arg:tt)*) => {{
         let color_code = format!("\x1b[38;2;{};{};{}m", $r, $g, $b);
         $crate::apply_color!(&color_code, $($arg)*)
+    }};
+}
+
+#[macro_export]
+/// Applies HSL color to the provided format string.
+///
+/// # Arguments
+///
+/// * `h` - The hue (0-360).
+/// * `s` - The saturation (0-1).
+/// * `l` - The lightness (0-1).
+/// * `args` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", hsl!(120.0, 1.0, 0.5, "This is green text"));
+/// ```
+macro_rules! hsl {
+    ($h:expr, $s:expr, $l:expr, $($arg:tt)*) => {{
+        let (r, g, b) = $crate::hsl_to_rgb($h, $s, $l);
+        $crate::rgb!(r, g, b, $($arg)*)
+    }};
+}
+
+#[macro_export]
+/// Applies HSV color to the provided format string.
+///
+/// # Arguments
+///
+/// * `h` - The hue (0-360).
+/// * `s` - The saturation (0-1).
+/// * `v` - The value (0-1).
+/// * `args` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", hsv!(240.0, 1.0, 1.0, "This is blue text"));
+/// ```
+macro_rules! hsv {
+    ($h:expr, $s:expr, $v:expr, $($arg:tt)*) => {{
+        let (r, g, b) = $crate::hsv_to_rgb($h, $s, $v);
+        $crate::rgb!(r, g, b, $($arg)*)
+    }};
+}
+/// Applies a red background color to the provided format string.
+///
+/// # Arguments
+///
+/// * `$arg` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", bg_red!("This has a red background"));
+/// ```
+#[macro_export]
+macro_rules! bg_red {
+    ($($arg:tt)*) => {{
+        $crate::apply_color!("\x1b[41m", $($arg)*)
+    }};
+}
+
+/// Applies a green background color to the provided format string.
+///
+/// # Arguments
+///
+/// * `$arg` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", bg_green!("This has a green background"));
+/// ```
+#[macro_export]
+macro_rules! bg_green {
+    ($($arg:tt)*) => {{
+        $crate::apply_color!("\x1b[42m", $($arg)*)
+    }};
+}
+
+/// Applies a blue background color to the provided format string.
+///
+/// # Arguments
+///
+/// * `$arg` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", bg_blue!("This has a blue background"));
+/// ```
+#[macro_export]
+macro_rules! bg_blue {
+    ($($arg:tt)*) => {{
+        $crate::apply_color!("\x1b[44m", $($arg)*)
+    }};
+}
+
+/// Applies a white background color to the provided format string.
+///
+/// # Arguments
+///
+/// * `$arg` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", bg_white!("This has a white background"));
+/// ```
+#[macro_export]
+macro_rules! bg_white {
+    ($($arg:tt)*) => {{
+        $crate::apply_color!("\x1b[47m", $($arg)*)
+    }};
+}
+
+/// Applies a black background color to the provided format string.
+///
+/// # Arguments
+///
+/// * `$arg` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", bg_black!("This has a black background"));
+/// ```
+#[macro_export]
+macro_rules! bg_black {
+    ($($arg:tt)*) => {{
+        $crate::apply_color!("\x1b[40m", $($arg)*)
+    }};
+}
+
+/// Applies a yellow background color to the provided format string.
+///
+/// # Arguments
+///
+/// * `$arg` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", bg_yellow!("This has a yellow background"));
+/// ```
+#[macro_export]
+macro_rules! bg_yellow {
+    ($($arg:tt)*) => {{
+        $crate::apply_color!("\x1b[43m", $($arg)*)
+    }};
+}
+
+/// Applies a magenta background color to the provided format string.
+///
+/// # Arguments
+///
+/// * `$arg` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", bg_magenta!("This has a magenta background"));
+/// ```
+#[macro_export]
+macro_rules! bg_magenta {
+    ($($arg:tt)*) => {{
+        $crate::apply_color!("\x1b[45m", $($arg)*)
+    }};
+}
+
+/// Applies a cyan background color to the provided format string.
+///
+/// # Arguments
+///
+/// * `$arg` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", bg_cyan!("This has a cyan background"));
+/// ```
+#[macro_export]
+macro_rules! bg_cyan {
+    ($($arg:tt)*) => {{
+        $crate::apply_color!("\x1b[46m", $($arg)*)
+    }};
+}
+
+/// Applies bold formatting to the provided format string.
+///
+/// # Arguments
+///
+/// * `$arg` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", bold!("This text is bold"));
+/// ```
+#[macro_export]
+macro_rules! bold {
+    ($($arg:tt)*) => {{
+        $crate::apply_color!("\x1b[1m", $($arg)*)
+    }};
+}
+
+/// Applies italic formatting to the provided format string.
+///
+/// # Arguments
+///
+/// * `$arg` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", italic!("This text is italic"));
+/// ```
+#[macro_export]
+macro_rules! italic {
+    ($($arg:tt)*) => {{
+        $crate::apply_color!("\x1b[3m", $($arg)*)
+    }};
+}
+
+/// Applies underline formatting to the provided format string.
+///
+/// # Arguments
+///
+/// * `$arg` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", underline!("This text is underlined"));
+/// ```
+#[macro_export]
+macro_rules! underline {
+    ($($arg:tt)*) => {{
+        $crate::apply_color!("\x1b[4m", $($arg)*)
+    }};
+}
+
+/// Applies a custom RGB background color to the provided format string.
+///
+/// # Arguments
+///
+/// * `$r` - The red component (0-255).
+/// * `$g` - The green component (0-255).
+/// * `$b` - The blue component (0-255).
+/// * `$arg` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", bg_rgb!(100, 150, 200, "This has a custom RGB background"));
+/// ```
+#[macro_export]
+macro_rules! bg_rgb {
+    ($r:expr, $g:expr, $b:expr, $($arg:tt)*) => {{
+        let color_code = format!("\x1b[48;2;{};{};{}m", $r, $g, $b);
+        $crate::apply_color!(&color_code, $($arg)*)
+    }};
+}
+
+/// Applies a background color specified in HSL color space to the provided format string.
+///
+/// # Arguments
+///
+/// * `$h` - The hue component (0-360).
+/// * `$s` - The saturation component (0-1).
+/// * `$l` - The lightness component (0-1).
+/// * `$arg` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", bg_hsl!(180.0, 0.5, 0.5, "This has an HSL-specified background"));
+/// ```
+#[macro_export]
+macro_rules! bg_hsl {
+    ($h:expr, $s:expr, $l:expr, $($arg:tt)*) => {{
+        let (r, g, b) = $crate::hsl_to_rgb($h, $s, $l);
+        $crate::bg_rgb!(r, g, b, $($arg)*)
+    }};
+}
+
+/// Applies a background color specified in HSV color space to the provided format string.
+///
+/// # Arguments
+///
+/// * `$h` - The hue component (0-360).
+/// * `$s` - The saturation component (0-1).
+/// * `$v` - The value component (0-1).
+/// * `$arg` - The format string and its arguments.
+///
+/// # Example
+///
+/// ```
+/// use term_ansi::*;
+///
+/// println!("{}", bg_hsv!(270.0, 0.7, 0.9, "This has an HSV-specified background"));
+/// ```
+#[macro_export]
+macro_rules! bg_hsv {
+    ($h:expr, $s:expr, $v:expr, $($arg:tt)*) => {{
+        let (r, g, b) = $crate::hsv_to_rgb($h, $s, $v);
+        $crate::bg_rgb!(r, g, b, $($arg)*)
     }};
 }
--- src/test.rs
+++ src/test.rs
@@ -1,15 +1,17 @@
-use crate::{black, blue, colour, cyan, green, magenta, red, white, yellow};
+use crate::{
+    bg_green, bg_hsl, bg_hsv, black, blue, bold, cyan, green, hsl, magenta, red, rgb, white, yellow,
+};
 
 #[test]
 fn test_simple_color() {
-    assert_eq!(red!("Hello"), "\x1b[31mHello\x1b[37m");
+    assert_eq!(red!("Hello"), "\x1b[31mHello\x1b[0m\u{1b}[37m");
 }
 
 #[test]
 fn test_nested_colors() {
     assert_eq!(
         white!("This is {} with {} color", red!("red"), green!("green")),
-        "\x1b[37mThis is \x1b[31mred\x1b[37m with \x1b[32mgreen\x1b[37m color\x1b[37m"
+        "\x1b[37mThis is \x1b[31mred\x1b[0m\x1b[37m with \x1b[32mgreen\x1b[0m\x1b[37m color\x1b[0m\u{1b}[37m"
     );
 }
 
@@ -21,7 +23,7 @@ fn test_complex_nesting() {
             red!("A"),
             green!("B with {}", blue!("C"))
         ),
-        "\x1b[37mOuter \x1b[31mA\x1b[37m, Inner \x1b[32mB with \x1b[34mC\x1b[32m\x1b[37m\u{1b}[37m"
+        "\x1b[37mOuter \x1b[31mA\x1b[0m\x1b[37m, Inner \x1b[32mB with \x1b[34mC\x1b[0m\x1b[32m\x1b[0m\x1b[37m\x1b[0m\u{1b}[37m"
     );
 }
 
@@ -29,7 +31,7 @@ fn test_complex_nesting() {
 fn test_multiple_colors_in_line() {
     assert_eq!(
         red!("Error: {}, {}", blue!("File not found"), green!("Please check your path")),
-        "\x1b[31mError: \x1b[34mFile not found\x1b[31m, \x1b[32mPlease check your path\x1b[31m\x1b[37m"
+        "\x1b[31mError: \x1b[34mFile not found\x1b[0m\x1b[31m, \x1b[32mPlease check your path\x1b[0m\x1b[31m\x1b[0m\u{1b}[37m"
     );
 }
 
@@ -37,42 +39,74 @@ fn test_multiple_colors_in_line() {
 fn test_color_reset() {
     assert_eq!(
         red!("Red text {} and reset", green!("Green text")),
-        "\x1b[31mRed text \x1b[32mGreen text\x1b[31m and reset\x1b[37m"
+        "\x1b[31mRed text \x1b[32mGreen text\x1b[0m\x1b[31m and reset\x1b[0m\u{1b}[37m"
     );
 }
 
 #[test]
 fn test_black_color() {
-    assert_eq!(black!("Black text"), "\x1b[30mBlack text\x1b[37m");
+    assert_eq!(black!("Black text"), "\x1b[30mBlack text\x1b[0m\u{1b}[37m");
 }
 
 #[test]
 fn test_yellow_color() {
-    assert_eq!(yellow!("Yellow text"), "\x1b[33mYellow text\x1b[37m");
+    assert_eq!(yellow!("Yellow text"), "\x1b[33mYellow text\x1b[0m\u{1b}[37m");
 }
 
 #[test]
 fn test_magenta_color() {
-    assert_eq!(magenta!("Magenta text"), "\x1b[35mMagenta text\x1b[37m");
+    assert_eq!(magenta!("Magenta text"), "\x1b[35mMagenta text\x1b[0m\u{1b}[37m");
 }
 
 #[test]
 fn test_cyan_color() {
-    assert_eq!(cyan!("Cyan text"), "\x1b[36mCyan text\x1b[37m");
+    assert_eq!(cyan!("Cyan text"), "\x1b[36mCyan text\x1b[0m\u{1b}[37m");
 }
 
 #[test]
-fn test_colour_color() {
+fn test_rgb_color() {
     assert_eq!(
-        colour!(255, 0, 0, "Red RGB"),
-        "\x1b[38;2;255;0;0mRed RGB\x1b[37m"
+        rgb!(255, 0, 0, "Red RGB"),
+        "\x1b[38;2;255;0;0mRed RGB\x1b[0m\u{1b}[37m"
     );
     assert_eq!(
-        colour!(0, 255, 0, "Green RGB"),
-        "\x1b[38;2;0;255;0mGreen RGB\x1b[37m"
+        rgb!(0, 255, 0, "Green RGB"),
+        "\x1b[38;2;0;255;0mGreen RGB\x1b[0m\u{1b}[37m"
     );
     assert_eq!(
-        colour!(0, 0, 255, "Blue RGB"),
-        "\x1b[38;2;0;0;255mBlue RGB\x1b[37m"
+        rgb!(0, 0, 255, "Blue RGB"),
+        "\x1b[38;2;0;0;255mBlue RGB\x1b[0m\u{1b}[37m"
+    );
+}
+
+#[test]
+fn test_bg_hsl() {
+    assert_eq!(
+        bg_hsl!(120.0, 1.0, 0.5, "HSL Green background"),
+        "\x1b[48;2;0;255;0mHSL Green background\x1b[0m\u{1b}[37m"
+    );
+}
+
+#[test]
+fn test_bg_hsv() {
+    assert_eq!(
+        bg_hsv!(240.0, 1.0, 1.0, "HSV Blue background"),
+        "\x1b[48;2;0;0;255mHSV Blue background\x1b[0m\u{1b}[37m"
+    );
+}
+
+#[test]
+fn test_nested_formatting() {
+    assert_eq!(
+        red!("{}", bg_green!("{}", bold!("Red text on green background"))),
+        "\x1b[31m\x1b[42m\x1b[1mRed text on green background\x1b[0m\x1b[42m\x1b[0m\x1b[31m\x1b[0m\u{1b}[37m"
+    );
+}
+
+#[test]
+fn test_nested_hsl_hsv() {
+    assert_eq!(
+        hsl!(0.0, 1.0, 0.5, "{}", bg_hsv!(120.0, 1.0, 1.0, "Red text on green background")),
+        "\x1b[38;2;255;0;0m\x1b[48;2;0;255;0mRed text on green background\x1b[0m\x1b[38;2;255;0;0m\x1b[0m\u{1b}[37m"
     );
 }
